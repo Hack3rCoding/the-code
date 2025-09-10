78d93eb1b00bf975f6c056f02763bfe8d2b5ade8
Hack3rCoding <ibrahimhack3r101@gmail.com>
2025-09-10 19:23:31 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.showString("Happy")
+basic.pause(100)
+basic.clearScreen()
+basic.showString("BirthDay")
